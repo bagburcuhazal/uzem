93fc437b6cffd6f9fa8b2c5ad3bbeefc6eef8be6
hzlbrc <bagburcu@hotmail.com>
2014-08-13 18:10:01 +0300

    kamp ilk uygulama

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Uzem::Application.config.secret_key_base = 'cc1b3a89bff1ffad07615d2cfdb76ce114a82d3897489bd087141af765e731187b4704e30f74eb95c6c76b0eaa18c6abfd1147b647dd704df96c41ccaea738f0'
